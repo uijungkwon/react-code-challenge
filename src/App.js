2b95a62742aeb4cb3f8c8ecf92c5a63b5ee80fe6
uijung kwon <kuj08036410@gmail.com>
2023-06-05 10:29:08 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import styled from "styled-components";
+
+const Box = styled.div`
+    backgroundColor:"red",
+    width:200,
+    height:200,
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+export default App;
